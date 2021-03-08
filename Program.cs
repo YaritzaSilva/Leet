08f2ecb4e846c19a7fd7fc91acb7f84adbe5eb82
Yaritza <yaritzamicaelleb@gmail.com>
2021-03-08 20:05:28 -0300

    Tarefa Leet

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textoPadrão;
+
+            Console.Write("Texto a ser traduzido: ");
+            textoPadrão= Console.ReadLine();
+
+            string textoModificado= textoPadrão.Replace("A","4");
+            textoModificado=textoModificado.Replace("a","4");
+            textoModificado=textoModificado.Replace("E","3");
+            textoModificado=textoModificado.Replace("e","3");
+            textoModificado=textoModificado.Replace("I","1");
+            textoModificado=textoModificado.Replace("i","1");
+            textoModificado=textoModificado.Replace("L","1");
+            textoModificado=textoModificado.Replace("l","1");
+            textoModificado=textoModificado.Replace("O","0");
+            textoModificado=textoModificado.Replace("o","0");
+            textoModificado=textoModificado.Replace("T","7");
+            textoModificado=textoModificado.Replace("t","7");
+            textoModificado=textoModificado.Replace("S","5");
+            textoModificado=textoModificado.Replace("s","5");
+
+
+           Console.WriteLine($"Seu texto:{textoModificado}");
+        }
+    }
+}
